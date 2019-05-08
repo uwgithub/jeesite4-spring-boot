751d5dd6d8af13318045f31b4c29ea54ad7e1cd3
Shen Haibing <haifei201510@163.com>
2019-05-08 09:07:40 +0800

    pom和test任务更新

--- jeesite-web/src/test/java/com/jeesite/test/Test.java
+++ jeesite-web/src/test/java/com/jeesite/test/Test.java
@@ -1,9 +1,36 @@
 package com.jeesite.test;
 
-public class Test {
 
-	public static void main(String[] args) {
-		
-	}
-	
+/**
+ *  java类简单作用描述
+ * @ProjectName:
+ * @Package:        com.jeesite.test
+ * @ClassName:      Test
+ * @Description:    java类作用描述
+ * @Author:         作者姓名
+ * @CreateDate:     2019/5/8 1:04
+ * @UpdateUser:     作者姓名
+ * @UpdateDate:     2019/5/8 1:04
+ * @UpdateRemark:   The modified content
+ * @Version:        1.0
+ * <p>Copyright: Copyright (c) 2019/5/8</p>
+ *
+ */
+public class Test{
+    /**
+     * 方法实现说明
+     *
+     * @return 
+     * @throws
+     * @method a
+     * @author 者姓名
+     * @version a
+     * @date 2019/5/8 1:3
+     * @see 对类、属性、方法说明 参考转向
+     * @param
+     */
+
+    public void ssT(String orig,String dest){
+
+    }
 }
